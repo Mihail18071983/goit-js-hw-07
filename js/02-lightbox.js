e9b68147d45f389f02a9b4badc1c7d9b7076957d
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-10-22 18:00:29 +0300

    hw7 task2
    instal pakage

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const refs = {
+  imageContainer: document.querySelector(".gallery"),
+  body: document.body,
+};
+
+const cardgalleryMarkup = makegalleryItems(galleryItems);
+
+refs.imageContainer.insertAdjacentHTML("beforeend", cardgalleryMarkup);
+
+function makegalleryItems(items) {
+  return items
+    .map(({ preview, description, original }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>`;
+    })
+    .join("");
+}

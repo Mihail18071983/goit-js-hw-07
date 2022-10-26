1f0c936071041dc65824c17400c60f31b1c56a66
Mihail18071983 <106537730+Mihail18071983@users.noreply.github.com>
2022-10-26 17:40:06 +0300

    Update 02-lightbox.js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -13,7 +13,7 @@ refs.imageContainer.insertAdjacentHTML("beforeend", cardgalleryMarkup);
 function makegalleryItems(items) {
   return items
     .map(({ preview, description, original }) => {
-      return `<li class="gallery__item"><a class="gallery__item" href="${original}">
+      return `<li class="gallery__item"><a class="gallery__link" href="${original}">
   <img class="gallery__image" src="${preview}" alt="${description}" />
 </a></li>`;
     })
